a59935f20680ed0a9ad6774f4349084fb460830d
Aman Kumar Mandal <aman.mandal.71334@gmail.com>
2024-04-26 12:38:36 +0545

    Made the Navbar and Footer basics

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        Navbar
+    <nav className='bg-[#260d3c] text-[white] flex justify-between items-center h-16 px-6'>
+        <div className='logo font-bold'>GetMeAChai!</div>
+        <ul className='flex gap-5'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Sign Up</li>
+            <li>Login</li>
+        </ul>
     </nav>
   )
 }
